28725f370cc42729cbe0873b38e5b15854be589b
Student030 <student030@Eng-01.uwimona.edu.jm>
2018-11-12 16:53:11 -0500

    Exercise 3

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var button;
-
 var httpRequest = new XMLHttpRequest();
 
 window.onload = function (){
@@ -11,18 +10,19 @@ function Click(){
     
     var url = "https://info2180-lab6-lanai.c9users.io/request.php?q=";
     var text = document.getElementById("SubmitField").value;
-    httpRequest.onreadystatechange = doSomething;
+    httpRequest.onreadystatechange = doSomething(text);
     
     httpRequest.open('GET', url + text);
     httpRequest.send();
 }
 
 
-function doSomething() {
+function doSomething(text) {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
         if (httpRequest.status === 200) {
             var response = httpRequest.responseText;
-            alert(response);
+            var result = document.getElementById("result");
+            result.innerHTML = response;
         }
         else {
             alert('There was a problem with the request.');
