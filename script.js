58941966bf6934245af7db86c6c9d3e8f5b68621
LanaiNev <43979368+LanaiNev@users.noreply.github.com>
2018-11-19 16:00:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 var button;
+var getAll;
 var httpRequest = new XMLHttpRequest();
 
 window.onload = function (){
     button = document.getElementById("Search");
+    getAll = document.getElementById("MoreDef");
+    
     button.onclick = function() {Click ();}
+    getAll.onclick = function() {All ();}
 }
 
 function Click(){
-    
     var url = "https://info2180-lab6-lanai.c9users.io/request.php?q=";
+    
     var text = document.getElementById("SubmitField").value;
-    httpRequest.onreadystatechange = doSomething(text);
+    httpRequest.onreadystatechange = doSomething();
     
     httpRequest.open('GET', url + text);
     httpRequest.send();
 }
 
+function All(){
+    var url = "https://info2180-lab6-lanai.c9users.io/request.php?all=true";
+    
+    httpRequest.onreadystatechange = doSomething();
+    
+    httpRequest.open('GET', url);
+    httpRequest.send();
+}
 
-function doSomething(text) {
+
+function doSomething() {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
         if (httpRequest.status === 200) {
             var response = httpRequest.responseText;
